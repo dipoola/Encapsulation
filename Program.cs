8b495b9c46d54203beae41dfefcefa9d25f05ac6
Dipps <Dipps@DESKTOP-IT87NAB>
2023-08-27 15:59:47 -1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+       //Encapsulation
+     Person person = new Person("MUmbarak", "Basit",2010 );
+     person.FullName();
+     person.CalcAge();
+     
+
+        }
+        class Person
+        {
+            private string LastName { get; set; }
+            private string FirstName { get; set; }
+            private int YearOfBirth { get; set; }
+            public Person(string lastName, string firstName, int yearOfBirth)
+            {
+                LastName = lastName;
+                FirstName = firstName;
+                YearOfBirth = yearOfBirth;
+            }
+            public void FullName()
+            {
+                Console.WriteLine($"The firstname is {FirstName} and the lastname is {LastName}.");
+            }
+            public void CalcAge()
+            {
+                int currentYear = 2023;
+                int age = currentYear - YearOfBirth;
+                Console.WriteLine($"The age is {age}");
+            }
+        }
+
+
+
+    }
+}
